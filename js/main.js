298ed02385910506ebc177fb5e36ae3bcd035ebc
Arthurwebdel <113822476+Arthurwebdel@users.noreply.github.com>
2023-01-30 09:35:37 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('active');
+        
+        });
+        $('.close-btn').on('click', function(){
+        $('.menu').removeClass('active');
+            
+            });
+})
